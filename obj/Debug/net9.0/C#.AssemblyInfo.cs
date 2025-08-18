cf40b11c1c9bc15861488394bea907866edbfc0a
Mwawaka <kedskeds90@gmail.com>
2025-08-18 14:41:23 +0300

    update:namespaces

--- obj/Debug/net9.0/C#.AssemblyInfo.cs
+++ obj/Debug/net9.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cbaae213e74f9695695972b02f9753f347c4163")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c079cbe77c7a965c0b6105b792ade3a7a12cfa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
