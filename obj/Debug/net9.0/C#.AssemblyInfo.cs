41e32c7a8650398c31481657ef76778b3cf9f38b
Mwawaka <kedskeds90@gmail.com>
2025-08-19 09:23:18 +0300

    update: sets of equality

--- obj/Debug/net9.0/C#.AssemblyInfo.cs
+++ obj/Debug/net9.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baa171122695bfb720b7636e2a2989f03c8344b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29dc909befa4a486febb3d27e2424c80ca8c50ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
