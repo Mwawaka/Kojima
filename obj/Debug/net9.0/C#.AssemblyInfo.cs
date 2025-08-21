3f373b75759b5f1d0496195a0a9524e0fa6ce47f
Mwawaka <kedskeds90@gmail.com>
2025-08-21 14:07:25 +0300

    update: structs

--- obj/Debug/net9.0/C#.AssemblyInfo.cs
+++ obj/Debug/net9.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37c0ea6a46c88b95da1d5d6d84921e630903817b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9d572a1c8d64a2ac7fa4707fae0555f97a5add1")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
