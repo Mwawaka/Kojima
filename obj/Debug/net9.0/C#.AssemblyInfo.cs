72564ebb5b8f7525cbbf45b48c65675c75c3ae94
Mwawaka <kedskeds90@gmail.com>
2025-08-22 13:36:25 +0300

    update: Resistor Color

--- obj/Debug/net9.0/C#.AssemblyInfo.cs
+++ obj/Debug/net9.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+160b8e07c4a24c315e01ca44d0c286b6bf5cfa69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23f6adc8d231ef605035d0f35a6125e9566d9a2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
