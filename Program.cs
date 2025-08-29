1b3101fa8e37b27dd6b361519efe791228afd094
Mwawaka <kedskeds90@gmail.com>
2025-08-29 11:50:59 +0300

    update: simple rotation

--- Program.cs
+++ Program.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class SimpleRotation : MonoBehaviour
 {
+    [Header("Rotato")]
     public Vector3 RotationPerSecond;
     void Update()
     {
         transform.Rotate(RotationPerSecond * Time.deltaTime, 2.3f);
+
     }
 }
