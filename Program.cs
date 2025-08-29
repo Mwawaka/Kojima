7ac6b6d0c7c80ab16b51b7fdbb8699fdd27e8f8a
Mwawaka <kedskeds90@gmail.com>
2025-08-29 11:12:04 +0300

    update: simple rotation

--- Program.cs
+++ Program.cs
@@ -1,38 +1,10 @@
-using JetBrains.Annotations;
 using UnityEngine;
 
-public class Item
+public class SimpleRotation : MonoBehaviour
 {
-    public string Name;
-    public int Worth;
-    public bool CanBeSold;
-    public Item(string name, string worth, string canBeSold)
-    {
-        Name = name;
-        Worth = worth;
-        CanBeSold = canBeSold;
-    }
-}
-public class Junta : MonoBehaviour
-{
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        Item item = new Item();
-        item.Name = "Junta";
-        item.Worth = 4;
-        item.CanBeSold = true;
-        Debug.Log($"This {item.Name} is worth {item.Worth} golden coins");
-
-    }
-
-    // Update is called once per frame
+    public Vector3 RotationPerSecond;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("Going to space ASAP");
-        }
+        transform.Rotate(RotationPerSecond * Time.deltaTime);
     }
 }
