10a76d0f2e33c8ae3e7da66540e781c8b7660b35
Mwawaka <kedskeds90@gmail.com>
2025-08-29 12:29:17 +0300

    update: simple rotation

--- Program.cs
+++ Program.cs
@@ -31,20 +31,36 @@ public class Equipment : Item
 {
     public int CurrentDurability = 100;
     public int MaximumDurability = 100;
+    public Equipment(int worth, string name, bool canBeSold, int maximumDurability) : base(worth, name, canBeSold)
+    {
+        MaximumDurability = maximumDurability;
+        CurrentDurability = MaximumDurability;
+    }
 }
 public class Weapon : Equipment
 {
     public WeaponType Type = WeaponType.Sword;
     public int MinDamage = 1;
     public int MaxDamage = 4;
-    public float attackTime = .6f;
-}
+    public float AttackTime = .6f;
 
+    public Weapon(int worth, string name, bool canBeSold, int maximumDurability, WeaponType type, int minDamage, int maxDamage, float attackTime) : base(worth, name, canBeSold, maximumDurability)
+    {
+        Type = type;
+        MinDamage = minDamage;
+        MaxDamage = maxDamage;
+        AttackTime = attackTime;
+    }
+}
 public class Armor : Equipment
 {
-    public int Defense = 1;
     public ArmorType Type = ArmorType.Helmet;
-
+    public int Defense = 1;
+    public Armor(int worth, string name, bool canBeSold, int maximumDurability, ArmorType type, int defense) : base(worth, name, canBeSold, maximumDurability)
+    {
+        Type = type;
+        Defense = defense;
+    }
 }
 public class SimpleRotation : MonoBehaviour
 {
