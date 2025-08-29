56e539515366d475a3b837ef191eda3288da66df
Mwawaka <kedskeds90@gmail.com>
2025-08-29 12:03:16 +0300

    update: simple rotation

--- Program.cs
+++ Program.cs
@@ -1,5 +1,23 @@
 using UnityEngine;
 
+public class Item
+{
+    public int Worth;
+    public string Name;
+    public bool CanBeSold;
+
+    public Item(int worth, string name, bool canBeSold)
+    {
+        Worth = worth;
+        Name = name;
+        CanBeSold = canBeSold;
+    }
+}
+public class Equipment : Item
+{
+    public int CurrentDurability;
+    public int MaximumDurability;
+}
 public class SimpleRotation : MonoBehaviour
 {
     [Header("Rotato")]
