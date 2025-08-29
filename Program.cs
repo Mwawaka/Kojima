5b792b8c7e3112c3958e35117427f65b7a6db3f0
Mwawaka <kedskeds90@gmail.com>
2025-08-29 09:55:40 +0300

    update: frames are updated per second

--- Program.cs
+++ Program.cs
@@ -1,27 +1,16 @@
-public class GradeSchool
+using UnityEngine;
+
+public class Junta : MonoBehaviour
 {
-    private readonly IDictionary<string, int> _grade = new Dictionary<string, int>();
-    private readonly SortedSet<string> _roster = new SortedSet<string>();
-
-
-    public bool Add(string student, int grade)
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        if (_roster.Contains(student))
-        {
-            return false;
-        }
-        _roster.Add(student);
-        _grade[student] = grade;
-        return true;
+        Debug.Log("The game is starting");
     }
 
-    public IEnumerable<string> Roster()
+    // Update is called once per frame
+    void Update()
     {
-        return _roster;
-    }
-
-    public IEnumerable<string> Grade(int grade)
-    {
-        return _grade.Where(g => g.Value == grade).Select(g => g.Key);
+        Debug.Log("The frames are updated per second");
     }
 }
