b2533b57aa91f4cef9772fdb7a3240a47be6c10d
Mwawaka <kedskeds90@gmail.com>
2025-08-29 12:12:43 +0300

    update: simple rotation

--- Program.cs
+++ Program.cs
@@ -1,5 +1,19 @@
 using UnityEngine;
-
+public enum ArmorType
+{
+    Helmet,
+    Chest,
+    Gloves,
+    Belt,
+    Boots
+}
+public enum WeaponType
+{
+    Axe,
+    Sword,
+    Hammer,
+    Knife,
+}
 public class Item
 {
     public int Worth;
@@ -15,8 +29,22 @@ public class Item
 }
 public class Equipment : Item
 {
-    public int CurrentDurability;
-    public int MaximumDurability;
+    public int CurrentDurability = 100;
+    public int MaximumDurability = 100;
+}
+public class Weapon : Equipment
+{
+    public WeaponType Type = WeaponType.Sword;
+    public int MinDamage = 1;
+    public int MaxDamage = 4;
+    public float attackTime = .6f;
+}
+
+public class Armor : Equipment
+{
+    public int Defense = 1;
+    public ArmorType Type = ArmorType.Helmet;
+
 }
 public class SimpleRotation : MonoBehaviour
 {
