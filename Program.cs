15a11bf604c4c69b070937f6154f6fb8672e15d4
Mwawaka <kedskeds90@gmail.com>
2025-08-29 11:40:00 +0300

    update: simple rotation

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ public class SimpleRotation : MonoBehaviour
     public Vector3 RotationPerSecond;
     void Update()
     {
-        transform.Rotate(RotationPerSecond * Time.deltaTime, 2.3);
-
+        transform.Rotate(RotationPerSecond * Time.deltaTime, 2.3f);
     }
 }
