7cdab4c053d048564a2e70ba83aa263617aa8625
Mwawaka <kedskeds90@gmail.com>
2025-08-29 10:34:48 +0300

    update: keycode enum

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
+public class Item
+{
+    public string name = "Unnamed Item";
+    public int worth = 1;
+    public bool canBeSold = true;
+}
 public class Junta : MonoBehaviour
 {
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("The game is starting");
+        Item item = new Item();
+        item.name = "Junta";
+        item.worth = 4;
+        item.canBeSold = true;
     }
 
     // Update is called once per frame
